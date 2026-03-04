fb23915f46ea20664ea9a898cd6f3cfb71a816d1
Nathan Povo <17831160+nathanpovo@users.noreply.github.com>
2026-03-04 11:18:50 +0100

    Add install for WinDirStat

--- install.ps1
+++ install.ps1
@@ -19,3 +19,4 @@ winget install -i --exact --source winget --id Mozilla.Firefox.DeveloperEdition
 winget install -i --exact --source winget --id Nilesoft.Shell
 winget install -i --exact --source winget --id Docker.DockerDesktop
 winget install -i --exact --source winget --id Microsoft.AzureCLI
+winget install -i --exact --source winget --id WinDirStat.WinDirStat
